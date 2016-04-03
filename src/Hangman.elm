e4517e6e4c4c4bf1aeb133851d81d813f230094d
Robin Pokorný <me@robinpokorny.com>
2016-04-03 11:31:20 +0200

    Format code

--- src/Hangman.elm
+++ src/Hangman.elm
@@ -1,6 +1,5 @@
 module Hangman (init, view, update, Model, Action(RequestMore, Guess)) where
 
-
 import Char
 import String
 import Set exposing (Set)
@@ -15,13 +14,14 @@ import Html.Events as Events
 
 -- MODEL
 
+
 type alias Model =
-  { word: String
-  , used: Set Char 
+  { word : String
+  , used : Set Char
   }
 
 
-init: (Model, Effects Action)
+init : ( Model, Effects Action )
 init =
   ( { word = "hangman"
     , used = Set.empty
@@ -29,30 +29,32 @@ init =
   , getRandomWord
   )
 
+
 attemptsCount : Int
 attemptsCount =
   6
 
 
+
 -- UPDATE
 
-type Action =
-  Guess Char
+
+type Action
+  = Guess Char
   | RequestMore
   | NewWord (Maybe String)
 
 
-update : Action -> Model -> (Model, Effects Action)
+update : Action -> Model -> ( Model, Effects Action )
 update action model =
   case action of
     Guess c ->
       ( case (status model) of
           Guessing _ ->
-            { model | used = Set.insert (Char.toLower c) model.used}
+            { model | used = Set.insert (Char.toLower c) model.used }
 
           _ ->
             model
-
       , Effects.none
       )
 
@@ -82,81 +84,92 @@ decodeJson =
   Json.at [ "word" ] Json.string
 
 
-type Status =
-  Guessing Int
+type Status
+  = Guessing Int
   | Winner
   | Loser
 
 
-status: Model -> Status
+status : Model -> Status
 status model =
-  if (winner model)
-  then Winner
+  if (winner model) then
+    Winner
   else
     let
-      wrong = wrongAttempts model
+      wrong =
+        wrongAttempts model
     in
-      if (wrong < attemptsCount)
-      then Guessing (attemptsCount - wrong)
-      else Loser
+      if (wrong < attemptsCount) then
+        Guessing (attemptsCount - wrong)
+      else
+        Loser
 
 
-winner: Model -> Bool
+winner : Model -> Bool
 winner { word, used } =
   String.all (\c -> Set.member c used) word
 
 
-wrongAttempts: Model -> Int
+wrongAttempts : Model -> Int
 wrongAttempts { word, used } =
   word
-  |> String.toList
-  |> Set.fromList
-  |> Set.diff used
-  |> Set.size
+    |> String.toList
+    |> Set.fromList
+    |> Set.diff used
+    |> Set.size
+
 
 
 -- VIEW
+
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   div
     [ Attributes.class "splash" ]
     [ h1
-      [ Attributes.class "splash-head" ]
-      [ inputField address
-      , text (String.toUpper (showSelected model.word model.used))
-      ]
-    , p
-      [ Attributes.class "splash-subhead"]
-      [ message model ]
-    , p
-      []
-      [ button
-        [ Events.onClick address RequestMore
-        , Attributes.href "#"
-        , Attributes.class "pure-button pure-button-primary"
+        [ Attributes.class "splash-head" ]
+        [ inputField address
+        , text (String.toUpper (showSelected model.word model.used))
         ]
-        [ text "New word" ]
-      ]
+    , p
+        [ Attributes.class "splash-subhead" ]
+        [ message model ]
+    , p
+        []
+        [ button
+            [ Events.onClick address RequestMore
+            , Attributes.href "#"
+            , Attributes.class "pure-button pure-button-primary"
+            ]
+            [ text "New word" ]
+        ]
     ]
 
 
-message: Model -> Html
+message : Model -> Html
 message model =
   case (status model) of
-    Guessing remaining -> text ( "Attempts remaining " ++ (toString remaining))
-    Winner -> text "Winner!"
+    Guessing remaining ->
+      text ("Attempts remaining " ++ (toString remaining))
+
+    Winner ->
+      text "Winner!"
+
     Loser ->
-      span []
-        [ strong []
-          [ text "Loser! " ]
+      span
+        []
+        [ strong
+            []
+            [ text "Loser! " ]
         , text "Word was: "
-        , em []
-          [ text model.word ]
+        , em
+            []
+            [ text model.word ]
         ]
 
 
-inputField: Signal.Address Action -> Html
+inputField : Signal.Address Action -> Html
 inputField address =
   input
     [ onKeyUp address Guess
@@ -168,36 +181,40 @@ inputField address =
 
 onKeyUp : Signal.Address Action -> (Char -> Action) -> Attribute
 onKeyUp address handler =
-  Events.on "keyup" Events.keyCode
-    (\code -> code
-      |> Char.fromCode
-      |> handler
-      |> Signal.message address
+  Events.on
+    "keyup"
+    Events.keyCode
+    (\code ->
+      code
+        |> Char.fromCode
+        |> handler
+        |> Signal.message address
     )
 
 
-showSelected: String -> Set Char -> String
+showSelected : String -> Set Char -> String
 showSelected word selected =
   String.map
     (\c ->
-      if (Set.member c selected)
-      then c
-      else '_'
+      if (Set.member c selected) then
+        c
+      else
+        '_'
     )
     word
 
 
-wotdUrl: String
+wotdUrl : String
 wotdUrl =
   Http.url
     "http://api.wordnik.com:80/v4/words.json/randomWord"
-    [ ("hasDictionaryDef", "true")
-    , ("includePartOfSpeech", "noun")
-    , ("minCorpusCount", "100000")
-    , ("maxCorpusCount", "-1")
-    , ("minDictionaryCount", "1")
-    , ("maxDictionaryCount", "-1")
-    , ("minLength", "5")
-    , ("maxLength", "-1")
-    , ("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    [ ( "hasDictionaryDef", "true" )
+    , ( "includePartOfSpeech", "noun" )
+    , ( "minCorpusCount", "100000" )
+    , ( "maxCorpusCount", "-1" )
+    , ( "minDictionaryCount", "1" )
+    , ( "maxDictionaryCount", "-1" )
+    , ( "minLength", "5" )
+    , ( "maxLength", "-1" )
+    , ( "api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5" )
     ]
--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,40 @@
+module Main (..) where
+
 import Keyboard
 import Char
 import Task
 import StartApp
 import Html exposing (Html)
 import Effects exposing (Never)
-
 import Hangman exposing (init, view, update, Model)
 
 
-app: StartApp.App Model
+app : StartApp.App Model
 app =
-    StartApp.start
-      { init = init
-      , view = view
-      , update = update
-      , inputs = [ guessChar ]
-      }
+  StartApp.start
+    { init = init
+    , view = view
+    , update = update
+    , inputs = [ guessChar ]
+    }
 
-guessChar: Signal Hangman.Action
+
+guessChar : Signal Hangman.Action
 guessChar =
   Signal.map codeToAction Keyboard.presses
 
-codeToAction: Char.KeyCode -> Hangman.Action
+
+codeToAction : Char.KeyCode -> Hangman.Action
 codeToAction c =
   c
-  |> Char.fromCode
-  |> Hangman.Guess
+    |> Char.fromCode
+    |> Hangman.Guess
 
-main: Signal Html
+
+main : Signal Html
 main =
-    app.html
+  app.html
+
 
 port tasks : Signal (Task.Task Never ())
 port tasks =
